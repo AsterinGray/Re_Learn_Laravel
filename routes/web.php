0c1e1a4e41eca771ec3b79cfefc176adcb9678a3
AsterinGray <raymondtiandinatan@gmail.com>
2020-05-25 18:28:48 +0700

    Learned Laravel View, Route, & Templating

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    $name = 'Raymond Tiandinatan';
+    return view('about', ['name' => $name]);
 });
